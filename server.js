4288a9c837a150aa4c7f5fb8a66826755b9eb779
william5ouza <will.dasilva.souza@gmail.com>
2021-04-21 21:52:24 +0000

    testing

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connectDB = require('./connections/connector');
 const app = express();
 
 dotenv.config({path:'.env'});
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 //log requests
 app.use(morgan('tiny'));
